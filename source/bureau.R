776ac24cd19ffdf28c79e75c11421f34ff2f3fe4
Dmitry Petukhov <d.petukhov@outlook.com>
2018-07-18 10:26:13 +0000

    Update bureau funs

--- source/bureau.R
+++ source/bureau.R
@@ -13,8 +13,27 @@ source("common_fe.R")
 #'
 #' @param .config 
 #' 
-bureau._load <- function(.config, .fillNA) {
-  burea <- fread.csv.zipped("bureau.csv", .config$DataDir) %>% 
+bureau.load <- function(.config) {
+  stopifnot(is.list(.config))
+  burea <- fread.csv.zipped("bureau.csv", .config$DataDir)
+}
+
+
+
+#' 
+#'
+#' @param dt 
+#' @param .minObservationNumber 
+#'
+burea.filter <- function(dt, .minObservationNumber = 100L) {
+  require(dplyr)
+  stopifnot(
+    is.data.frame(dt),
+    is.numeric(.minObservationNumber)
+  )
+  
+  dt %>% 
+    ## prepare to filter
     mutate_if(
       is.character,
       funs(if_else(!is.na(.), str_replace_all(str_to_lower(.), "\\W", "_"), NA_character_))
@@ -22,8 +41,84 @@ bureau._load <- function(.config, .fillNA) {
     mutate(
       CreditCurrency = if_else(CreditCurrency == "currency_1", 0L, 1L)
     ) %>% 
+    ## filter by CreditActive type
     filter(
       CreditActive %in% c("closed", "active")
+    ) %>% 
+    ## filter rare cases
+    group_by(
+      CreditType, CreditActive, CreditCurrency
+    ) %>% 
+    filter(
+      n() >= .minObservationNumber
+    ) %>% 
+    ungroup()
+}
+
+
+
+#' 
+#'
+#' @param dt 
+#'
+burea.preprocessing <- function(dt) {
+  require(dplyr)
+  stopifnot(
+    is.data.frame(dt)
+  )
+
+  dt %>% 
+    mutate_at(
+      c("DaysCreditEnddate", "DaysEnddateFact", "DaysCreditUpdate"),
+      funs(DaysCredit_diff = . - DaysCredit)
+    ) %>%
+    mutate(
+      CreditDaysUnderdue = DaysCreditEnddate - DaysEnddateFact
+    )
+}
+
+
+
+#' 
+#' 
+#' @param .fillNA 
+#' @param balances 
+#' @param dt 
+#' 
+bureau.getHistoryStats <- function(dt, balances, .fillNA = NA_real_) {
+  require(dplyr)
+
+  stopifnot(
+    is.data.frame(dt),
+    is.data.frame(balances),
+    is.numeric(.fillNA)
+  )
+  
+  
+  keyField <- "SkIdCurr"
+  
+  ## 1
+  dt <- dt %>% 
+    # join w/ stats
+    left_join(balances, by = "SkIdBureau") %>% 
+    # order by days before current application did client apply for Credit Bureau credit
+    arrange(DaysCredit)
+  
+  
+  ## 2
+  values <- setdiff(names(dt %>% select_if(is.numeric)),
+                    c(keyField, "SkIdBureau", "CreditType", "CreditActive", "CreditCurrency"))
+  
+  dt %>% 
+    common.fe.calcStatsByGroups(.,
+                                keyField, 
+                                list(".", "CreditType", "CreditActive", c("CreditType", "CreditActive", "CreditCurrency")),
+                                values,
+                                .fillNA = NA_real_, .drop = T) %>%
+    # fill NAs if nessesary
+    mutate_if(
+      is.double,
+      funs(replace_na(., .fillNA))
     )
 }
 
@@ -31,7 +126,14 @@ bureau._load <- function(.config, .fillNA) {
 
 #' Get Bureau Balances stats
 #'
-bureau._getBalancesStats <- function(.config, .fillNA, .minSD, .minNA) {
+#' @param .config 
+#' @param .fillNA 
+#' @param .minSD 
+#' @param .minNA 
+#' 
+bureau.getBalancesStats <- function(.config, 
+                                    .fillNA = NA_real_,
+                                    .minSD = .01, .minNA = .1) {
   fread.csv.zipped("bureau_balance.csv", .config$DataDir) %>% 
     common.fe.calcStatsByGroups(.,
                                 "SkIdBureau", list(".", "Status"), "MonthsBalance",
@@ -46,27 +148,14 @@ bureau._getBalancesStats <- function(.config, .fillNA, .minSD, .minNA) {
 
 
 
-#' 
-#' @param .config 
-#' @param .minObservationNumber 
-#' @param .fillNA 
-#' @param .minNA 
-#' @param .minSD 
-#' 
-bureau.getHistoryStats <- function(.config, 
-                                   .fillNA = NA_real_,
-                                   .minObservationNumber = 100, 
-                                   .minSD = .01, .minNA = .1) {
+#'
+#'
+#' @param dt 
+#'
+bureau.featureSelection <- function(dt, .minSD = .01, .minNA = .01) {
   require(dplyr)
-  require(data.table)
-  require(purrr)
-  require(psych)
-  require(stringr)
-  
   stopifnot(
-    is.list(.config),
-    is.numeric(.fillNA),
-    is.numeric(.minObservationNumber),
+    is.data.frame(dt),
     is.numeric(.minSD),
     is.numeric(.minNA)
   )
@@ -74,40 +163,8 @@ bureau.getHistoryStats <- function(.config,
   
   keyField <- "SkIdCurr"
   
-  ## 1
-  dt <- bureau._load(.config, .fillNA) %>% 
-    # add features
-    mutate(
-      CreditDayUnderdue = DaysCreditEnddate - DaysEnddateFact
-    ) %>% 
-    # filter rare cases
-    group_by(
-      CreditType, CreditActive, CreditCurrency
-    ) %>% 
-    filter(
-      n() >= .minObservationNumber
-    ) %>% 
-    ungroup() %>% 
-    # join w/ stats
-    left_join(
-      bureau._getBalancesStats(.config, .fillNA, .minSD, .minNA),
-      by = "SkIdBureau"
-    ) %>% 
-    # order by days before current application did client apply for Credit Bureau credit
-    arrange(DaysCredit)
-  
-  
-  ## 2
-  values <- setdiff(names(dt %>% select_if(is.numeric)),
-                    c(keyField, "SkIdBureau", "CreditType", "CreditActive", "CreditCurrency"))
-  
   dt %>% 
-    common.fe.calcStatsByGroups(.,
-                                keyField, 
-                                list(".", "CreditType", "CreditActive", c("CreditType", "CreditActive", "CreditCurrency")),
-                                values,
-                                .fillNA = NA_real_, .drop = T) %>% 
-    # remove redundant fields by mask
+    ## remove redundant fields by mask
     select(
       -matches("_min_([[:alnum:]]_)?max"),
       -matches("_max_([[:alnum:]]_)?min"),
@@ -115,17 +172,12 @@ bureau.getHistoryStats <- function(.config,
       -matches("_(min|median|max)_([[:alnum:]]_)?length"),
       -matches("_first")
     ) %>% 
-    # remove redundant fields by stats
+    ## remove redundant fields by stats
     select(
       -one_of(common.fe.findRedundantCols(., .minSD, .minNA))
-    ) %>% 
+    ) %>%
     select(
       -one_of(common.fe.findCorrelatedCols(., .threshold = .9, .fillNA = 0, .extraFields = keyField))
-    ) %>% 
-    # fill NAs if nessesary
-    mutate_if(
-      is_double,
-      funs(replace_na(., .fillNA))
     )
 }
 
