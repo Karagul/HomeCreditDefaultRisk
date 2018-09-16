b47bdc7714bfeeaaddf0d600271b5f99850c77df
Dmitry Petukhov <d.petukhov@outlook.com>
2018-09-16 17:11:31 +0000

    Update download.sh

--- data/download.sh
+++ data/download.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+#
+# Download data vai Kaggle API instruction
+#
 
-# install (if nessesary)
-pip install kaggle
 
-# download
-chmod -R ugo+rw /home/<user>/apps/HomeCreditDefaultRisk/data
-kaggle competitions download -c home-credit-default-risk -p /home/<user>/apps/HomeCreditDefaultRisk/data
+usr = dp
+readonly usr
+
+
+# 1. install kaggle package (if necessary)
+pip install --user $usr kaggle
+
+# 2. upload kaggle.json to ~/.kaggle/kaggle.json (if necessary)
+
+# 3. download competitions data
+kaggle competitions download -c home-credit-default-risk -p /home/$usr/apps/HomeCreditDefaultRisk/data
+
+
+# References ----
+# 1. https://github.com/Kaggle/kaggle-api
